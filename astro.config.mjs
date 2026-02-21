6ffff765baab626a13fb6c2d0d5b38aecb559136
christianhw27 <dannyychr27@gmail.com>
2026-02-21 23:47:26 +0700

    benerin vercel ajg

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,16 +8,19 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dannychristian.com',
-vite: {
-    plugins: [
-      // @ts-expect-error
-      tailwindcss()
-    ]
-  },
 
-  integrations: [mdx(), react(), keystatic()]
-  
+  vite: {
+      plugins: [
+        // @ts-expect-error
+        tailwindcss()
+      ]
+    },
+
+  integrations: [mdx(), react(), keystatic()],
+  adapter: vercel()
 });
